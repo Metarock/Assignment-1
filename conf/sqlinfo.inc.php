d80658a08c74cbabf9d60189c3c6aca2f61f6a4d
John Isaiah Sangalang <John Sangalang>
2021-04-21 11:47:21 +1200

    added comments

--- conf/sqlinfo.inc.php
+++ conf/sqlinfo.inc.php
@@ -1,8 +1,8 @@
 <?php
 // Include file with sql details
-$host = "cmslamp14.aut.ac.nz";
-$user = "jsj6212";
-$password = "johnisaiah2000";
-$db = "jsj6212";
-$table = "statusform";
+$host = "cmslamp14.aut.ac.nz"; //the host for SQL
+$user = "jsj6212"; //username
+$password = "johnisaiah2000"; //password
+$db = "jsj6212"; //database name
+$table = "statusform" //table name;
 ?>
