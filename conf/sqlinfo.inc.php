8cdb331d43752996f88d4f1bbf1482d8019e01b6
John Isaiah Sangalang <John Sangalang>
2021-04-21 11:54:23 +1200

    minor changes

--- conf/sqlinfo.inc.php
+++ conf/sqlinfo.inc.php
@@ -4,5 +4,5 @@ $host = "cmslamp14.aut.ac.nz"; //the host for SQL
 $user = "jsj6212"; //username
 $password = "johnisaiah2000"; //password
 $db = "jsj6212"; //database name
-$table = "statusform" //table name;
+$table = "statusform"; //table name;
 ?>
